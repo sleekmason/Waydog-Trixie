323e1b84fe8a0c44a37b20d4dc3aba2ea98fd285
sleekmason <sleekmason@gmail.com>
2026-03-02 11:03:28 -0600

    Added Cursor highlighter to labwc (super + /) - Improved labwc-keys.sh for proper naming where necessary.

--- config/includes.chroot_after_packages/usr/local/bin/labwc-keys.sh
+++ config/includes.chroot_after_packages/usr/local/bin/labwc-keys.sh
@@ -2,9 +2,17 @@
 # Display labwc keybindings
 # Created for Waydog by sleekmason 17 Dec 2025
 
+declare -A FRIENDLY_NAMES=(
+  ["wl-find-cursor"]="Highlight Cursor"
+  ["grimshot save area"]="Screenshot Select Area"
+  ["labwc-keys.sh"]="Keybinds Labwc"
+  ["waybar-icon-toggle dialog"]="Waybar Options"
+  ["labwc --reconfigure"]="Reload labwc"
+  ["toggleShowDesktop"]="Show Desktop"
+)
+
 CONFIG="$HOME/.config/labwc/rc.xml"
 
-# Terminal preference
 TERMINALS=(
   "xfce4-terminal"
   "foot"
@@ -14,7 +22,6 @@ TERMINALS=(
   "kitty"
   "x-terminal-emulator"
 )
-
 TERM_CMD=""
 for term in "${TERMINALS[@]}"; do
   if command -v "$term" >/dev/null 2>&1; then
@@ -22,7 +29,6 @@ for term in "${TERMINALS[@]}"; do
     break
   fi
 done
-
 if [[ -z "$TERM_CMD" ]]; then
   notify-send "labwc-keys" "No terminal emulator found"
   exit 1
@@ -31,111 +37,66 @@ fi
 run_in_terminal() {
   SCRIPT="$1"
   case "$TERM_CMD" in
-    xfce4-terminal)
-      "$TERM_CMD" -x "$SCRIPT"
-      ;;
-    gnome-terminal|x-terminal-emulator)
-      "$TERM_CMD" -- "$SCRIPT"
-      ;;
-    kitty|alacritty|wezterm|foot)
-      "$TERM_CMD" -e "$SCRIPT"
-      ;;
-    *)
-      "$TERM_CMD" -e "$SCRIPT"
-      ;;
+    xfce4-terminal)   "$TERM_CMD" -x "$SCRIPT" ;;
+    gnome-terminal|x-terminal-emulator) "$TERM_CMD" -- "$SCRIPT" ;;
+    kitty|alacritty|wezterm|foot) "$TERM_CMD" -e "$SCRIPT" ;;
+    *) "$TERM_CMD" -e "$SCRIPT" ;;
   esac
 }
 
-# Temp script
+TMP_NAMES=$(mktemp)
+for key in "${!FRIENDLY_NAMES[@]}"; do
+  printf '%s\t%s\n' "$key" "${FRIENDLY_NAMES[$key]}"
+done > "$TMP_NAMES"
+
 TMP_SCRIPT=$(mktemp)
-
-cat >"$TMP_SCRIPT" <<'EOF'
+cat >"$TMP_SCRIPT" <<EOF
 #!/bin/bash
-CONFIG="$HOME/.config/labwc/rc.xml"
-
+CONFIG="\$HOME/.config/labwc/rc.xml"
+NAMES_FILE="$TMP_NAMES"
 clear
-
-# Basic colors
-YELLOW="\033[33m"
-GREEN="\033[32m"
-WHITE="\033[37m"
-CYAN="\033[36m"
 BLUE="\033[34m"
-MAGENTA="\033[35m"
+GREEN="\033[32m"
 RESET="\033[0m"
-
-echo -e " ${BLUE}LABWC KEYBINDS   Legend: W=Super  S=Shift  C=Ctrl${RESET}"
-echo -e " ${GREEN}--------------------------------------------------------${RESET}"
-
-# Hardcoded Alt+Tab Window Switcher at the top
+echo -e " \${BLUE}LABWC KEYBINDS   Legend: W=Super  S=Shift  C=Ctrl\${RESET}"
+echo -e " \${GREEN}----------------------------------------------------------\${RESET}"
 printf " \033[33m%-15s\033[0m  \033[32m%-15s\033[0m  \033[34m%s\033[0m\n" "Alt+Tab" "WindowSwitcher" "Switch Windows"
-
-awk '
-BEGIN {
-  KEY_COLOR=33     # yellow
-  ACT_COLOR=32     # green
-  DET_COLOR=34     # blue
-  RESET="\033[0m"
-
-  CKEY="\033[" KEY_COLOR "m"
-  CACT="\033[" ACT_COLOR "m"
-  CDET="\033[" DET_COLOR "m"
-}
-
+awk -F'\t' 'NR==FNR { names[\$1]=\$2; next }
 /<keybind key=/ {
-  key=$0
-  gsub(/.*key="/,"",key)
-  gsub(/".*/,"",key)
-  action=""
-  detail=""
+  key=\$0; gsub(/.*key="/,"",key); gsub(/".*/,"",key)
+  action=""; detail=""
 }
-
 /<action name=/ {
-  action=$0
-  gsub(/.*name="/,"",action)
-  gsub(/".*/,"",action)
+  action=\$0; gsub(/.*name="/,"",action); gsub(/".*/,"",action)
 }
-
 /<execute>/ {
-  detail=$0
-  gsub(/.*<execute>/,"",detail)
-  gsub(/<\/execute>.*/,"",detail)
+  detail=\$0; gsub(/.*<execute>/,"",detail); gsub(/<\/execute>.*/,"",detail)
 }
-
+/<command>/ {
+  detail=\$0; gsub(/.*<command>/,"",detail); gsub(/<\/command>.*/,"",detail)
+}
 /<to>/ {
-  detail=$0
-  gsub(/.*<to>/,"",detail)
-  gsub(/<\/to>.*/,"",detail)
+  detail=\$0; gsub(/.*<to>/,"",detail); gsub(/<\/to>.*/,"",detail)
   detail="Desktop " detail
 }
-
 /<\/keybind>/ {
+  CKEY="\033[33m"; CACT="\033[32m"; CDET="\033[34m"; CR="\033[0m"
+  if (detail in names) detail=names[detail]
   if (action=="GoToDesktop" && detail=="") {
-    if (key ~ /-[0-9]+$/)
-      detail="Desktop " substr(key, match(key, /[0-9]+$/))
-    else if (key ~ /Left$/)
-      detail="Desktop Left"
-    else if (key ~ /Right$/)
-      detail="Desktop Right"
+    if (key ~ /-[0-9]+$/) detail="Desktop " substr(key, match(key,/[0-9]+$/))
+    else if (key ~ /Left$/) detail="Desktop Left"
+    else if (key ~ /Right$/) detail="Desktop Right"
   }
-
   if (detail!="")
-    printf " %s%-15s%s  %s%-15s%s  %s%s%s\n",
-      CKEY, key, CRESET,
-      CACT, action, CRESET,
-      CDET, detail, CRESET
+    printf " %s%-15s%s  %s%-15s%s  %s%s%s\n", CKEY,key,CR, CACT,action,CR, CDET,detail,CR
   else
-    printf " %s%-15s%s  %s%-15s%s\n",
-      CKEY, key, CRESET,
-      CACT, action, CRESET
-}
-' "$CONFIG" | sort
-
-echo -e " ${GREEN}--------------------------------------------------------${RESET}"
+    printf " %s%-15s%s  %s%-15s%s\n", CKEY,key,CR, CACT,action,CR
+}' "\$NAMES_FILE" "\$CONFIG" | sort
+echo -e " \${GREEN}----------------------------------------------------------\${RESET}"
 echo
 read -n1 -s -r -p "Press any key to close..."
 echo
+rm -f "\$NAMES_FILE"
 EOF
-
 chmod +x "$TMP_SCRIPT"
 run_in_terminal "$TMP_SCRIPT"
